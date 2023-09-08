8ac4cf6ce7e9f19134705b0e66ab639d125a7ca7
ranganathnk <ranganathnkulkarni@gmail.com>
2023-09-08 12:48:28 +0530

    put added

--- src/server/routes/students.js
+++ src/server/routes/students.js
@@ -6,6 +6,7 @@ const app = express();
 const router = express.Router();
 
 app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 router.get("/students/all", (req, res) => {
   const sql = "select *from students";
@@ -39,6 +40,29 @@ router.get("/student/:rollNo", (req, res) => {
         status: 200,
         data: result,
         message: `Fetched data with user id ${req.params.rollNo}`,
+      });
+    }
+  });
+});
+
+router.put("/update/:rollNo/:name", (req, res) => {
+  const rollNo = req.params.rollNo;
+  const studentName = req.params.name;
+  const sql = `UPDATE students SET name='${studentName}' WHERE rollNo='${rollNo}'`;
+  con.query(sql, (error, result) => {
+    if (error) {
+      console.log(error);
+      res.status(400).json({
+        status: 400,
+        message: "Error updating user data",
+        result: result
+      });
+    } else {
+      // console.log(req.body)
+      res.status(200).json({
+        status: 200,
+        // responseData: req.body,
+        message: `Updated data with user id ${rollNo}`,
       });
     }
   });
